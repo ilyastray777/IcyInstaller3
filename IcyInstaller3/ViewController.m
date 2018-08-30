96543d118c801bd7d02ecf249ca684397fd1518d
MidnightChip <34963874+midnightchip@users.noreply.github.com>
2018-08-29 21:10:11 -0600

    Graceful closing of application

--- IcyInstaller3/ViewController.m
+++ IcyInstaller3/ViewController.m
@@ -29,6 +29,50 @@
 @property (strong, nonatomic) SearchViewController *searchViewController;
 @property (strong, nonatomic) ManageViewController *manageViewController;
 @end
+    
+//Graceful closuer
+@interface UIApplication (existing)
+- (void)suspend;
+- (void)terminateWithSuccess;
+@end
+@interface UIApplication (close)
+- (void)close;
+@end
+@implementation UIApplication (close)
+
+- (void)close
+{
+    // Check if the current device supports background execution.
+    BOOL multitaskingSupported = NO;
+    // iOS < 4.0 compatibility check.
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(isMultitaskingSupported)])
+        multitaskingSupported = [UIDevice currentDevice].multitaskingSupported;
+    // Good practice, we're using a private method.
+    if ([self respondsToSelector:@selector(suspend)])
+    {
+        if (multitaskingSupported)
+        {
+            [self beginBackgroundTaskWithExpirationHandler:^{}];
+            // Change the delay to your liking. I think 0.4 seconds feels just right (the "close" animation lasts 0.3 seconds).
+            [self performSelector:@selector(exit) withObject:nil afterDelay:0.4];
+        }
+        [self suspend];
+    }
+    else
+        [self exit];
+}
+
+- (void)exit
+{
+    // Again, good practice.
+    if ([self respondsToSelector:@selector(terminateWithSuccess)])
+        [self terminateWithSuccess];
+    else
+        exit(EXIT_SUCCESS);
+}
+
+@end
+//end close stuff
 
 @implementation ViewController
 UIButton *_aboutButton;
@@ -191,11 +235,15 @@ UIAlertView *respringAlert;
     if([_aboutButton.currentTitle isEqualToString:@"Dark"]) {
         [_aboutButton setTitle:@"Light" forState:UIControlStateNormal];
         [self switchToDarkMode];
-        exit(0);
+        [[UIApplication sharedApplication] close];
+        [[UIApplication sharedApplication] terminateWithSuccess];
+        
     } else if([_aboutButton.currentTitle isEqualToString:@"Light"]) {
         [_aboutButton setTitle:@"Dark" forState:UIControlStateNormal];
         [self switchToLightMode];
-        exit(0);
+        [[UIApplication sharedApplication] close];
+        [[UIApplication sharedApplication] terminateWithSuccess];
+        
     } else if([_aboutButton.currentTitle isEqualToString:@"Install"] && [SearchViewController getDepictionWebView].hidden) {
         [self messageWithTitle:@"Error" message:@"You need to search for a package first."];
     } else if([_aboutButton.currentTitle isEqualToString:@"Install"] && ![SearchViewController getDepictionWebView].hidden) {
