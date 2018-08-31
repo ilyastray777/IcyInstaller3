bb61a667bf69bd922224aa7825470f38fb9093ea
ArtikusHG <artiomoleynic@gmail.com>
2018-08-31 16:04:25 +0300

    Merge "graceful closing of application"
    Merged pull request "graceful closing of application" by MidnightChips

--- IcyInstaller3/ViewController.m
+++ IcyInstaller3/ViewController.m
@@ -30,7 +30,7 @@
 @property (strong, nonatomic) ManageViewController *manageViewController;
 @end
     
-//Graceful closuer
+// Graceful closuer (by midnightchips)
 @interface UIApplication (existing)
 - (void)suspend;
 - (void)terminateWithSuccess;
@@ -40,35 +40,27 @@
 @end
 @implementation UIApplication (close)
 
-- (void)close
-{
+- (void)close {
     // Check if the current device supports background execution.
     BOOL multitaskingSupported = NO;
     // iOS < 4.0 compatibility check.
-    if ([[UIDevice currentDevice] respondsToSelector:@selector(isMultitaskingSupported)])
-        multitaskingSupported = [UIDevice currentDevice].multitaskingSupported;
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(isMultitaskingSupported)]) multitaskingSupported = [UIDevice currentDevice].multitaskingSupported;
     // Good practice, we're using a private method.
-    if ([self respondsToSelector:@selector(suspend)])
-    {
-        if (multitaskingSupported)
-        {
+    if ([self respondsToSelector:@selector(suspend)]) {
+        if (multitaskingSupported) {
             [self beginBackgroundTaskWithExpirationHandler:^{}];
             // Change the delay to your liking. I think 0.4 seconds feels just right (the "close" animation lasts 0.3 seconds).
             [self performSelector:@selector(exit) withObject:nil afterDelay:0.4];
         }
         [self suspend];
     }
-    else
-        [self exit];
+    else [self exit];
 }
 
-- (void)exit
-{
+- (void)exit {
     // Again, good practice.
-    if ([self respondsToSelector:@selector(terminateWithSuccess)])
-        [self terminateWithSuccess];
-    else
-        exit(EXIT_SUCCESS);
+    if ([self respondsToSelector:@selector(terminateWithSuccess)]) [self terminateWithSuccess];
+    else exit(EXIT_SUCCESS);
 }
 
 @end
